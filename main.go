154c40852fa7ee6289bda1f0a9308e7a62fa6f6a
dbiesecke <dbiesecke@users.noreply.github.com>
2020-04-19 17:25:59 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,50 +7,11 @@ import (
 
 	//DNS Providers
 	_ "github.com/caddyserver/dnsproviders/acmedns"
-	_ "github.com/caddyserver/dnsproviders/alidns"
-	_ "github.com/caddyserver/dnsproviders/auroradns"
 	_ "github.com/caddyserver/dnsproviders/azure"
 	_ "github.com/caddyserver/dnsproviders/cloudflare"
-	_ "github.com/caddyserver/dnsproviders/cloudxns"
-	_ "github.com/caddyserver/dnsproviders/conoha"
-	_ "github.com/caddyserver/dnsproviders/digitalocean"
-	_ "github.com/caddyserver/dnsproviders/dnsimple"
-	_ "github.com/caddyserver/dnsproviders/dnsmadeeasy"
-	_ "github.com/caddyserver/dnsproviders/dnspod"
-	_ "github.com/caddyserver/dnsproviders/duckdns"
-	_ "github.com/caddyserver/dnsproviders/dyn"
-	_ "github.com/caddyserver/dnsproviders/exoscale"
-	_ "github.com/caddyserver/dnsproviders/fastdns"
-	_ "github.com/caddyserver/dnsproviders/gandi"
-	_ "github.com/caddyserver/dnsproviders/gandiv5"
-	_ "github.com/caddyserver/dnsproviders/generic"
-	_ "github.com/caddyserver/dnsproviders/glesys"
-	_ "github.com/caddyserver/dnsproviders/godaddy"
-	_ "github.com/caddyserver/dnsproviders/googlecloud"
-	_ "github.com/caddyserver/dnsproviders/httpreq"
-	_ "github.com/caddyserver/dnsproviders/inwx"
-	_ "github.com/caddyserver/dnsproviders/lightsail"
-	_ "github.com/caddyserver/dnsproviders/linode"
-	_ "github.com/caddyserver/dnsproviders/linodev4"
-	_ "github.com/caddyserver/dnsproviders/namecheap"
-	_ "github.com/caddyserver/dnsproviders/namedotcom"
-	_ "github.com/caddyserver/dnsproviders/namesilo"
-	_ "github.com/caddyserver/dnsproviders/nifcloud"
-	_ "github.com/caddyserver/dnsproviders/ns1"
-	_ "github.com/caddyserver/dnsproviders/otc"
-	_ "github.com/caddyserver/dnsproviders/ovh"
-	_ "github.com/caddyserver/dnsproviders/pdns"
-	_ "github.com/caddyserver/dnsproviders/rackspace"
-	_ "github.com/caddyserver/dnsproviders/rfc2136"
-	_ "github.com/caddyserver/dnsproviders/route53"
-	_ "github.com/caddyserver/dnsproviders/selectel"
-	_ "github.com/caddyserver/dnsproviders/stackpath"
-	_ "github.com/caddyserver/dnsproviders/transip"
-	_ "github.com/caddyserver/dnsproviders/vscale"
-	_ "github.com/caddyserver/dnsproviders/vultr"
+	
 
 	//Directives/Middleware
-	_ "blitznote.com/src/http.upload"                       //http.upload
 	_ "github.com/BTBurke/caddy-jwt"                        //http.jwt
 	_ "github.com/SchumacherFM/mailout"                     //http.mailout
 	_ "github.com/Xumeiquer/nobots"                         //http.nobots
@@ -70,8 +31,6 @@ import (
 	_ "github.com/jung-kurt/caddy-cgi"                      //http.cgi
 	_ "github.com/jung-kurt/caddy-pubsub"                   //http.pubsub
 	_ "github.com/linkonoid/caddy-dyndns"                   //http.dyndns
-	_ "github.com/lucaslorentz/caddy-supervisor/httpplugin" //http.supervisor
-	_ "github.com/lucaslorentz/caddy-supervisor/httpplugin" //http.supervisor
 	_ "github.com/mastercactapus/caddy-proxyprotocol"       //http.proxyprotocol
 	_ "github.com/miekg/caddy-prometheus"                   //http.prometheus
 	_ "github.com/nicolasazrak/caddy-cache"                 //http.cache
@@ -93,10 +52,10 @@ import (
 	_ "github.com/lucaslorentz/caddy-docker-proxy/plugin" //docker
 
 	//Server Types
-	_ "github.com/lucaslorentz/caddy-supervisor/servertype" //supervisor
+//	_ "github.com/lucaslorentz/caddy-supervisor/servertype" //supervisor
 
 	//TLS Clustering
-	_ "github.com/pteich/caddy-tlsconsul" //consul
+//	_ "github.com/pteich/caddy-tlsconsul" //consul
 
 	// Event Hooks
 	_ "github.com/hacdias/caddy-service" //hook.service issus: panic: close of closed channel
